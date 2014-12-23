c49cd2199825bdde586fa91a104c096e2f39317b
Brett Horner <hornerbrett@gmail.com>
2014-12-23 17:30:13 -0500

    New stuff + Yoga page

--- public/js/tonic_dev.js
+++ public/js/tonic_dev.js
@@ -1,2 +1,14 @@
-/*! tonic_dev 2014-12-18 */
-$(document).ready(function(){$("body").fadeIn(3e3),$("p").addClass("red")});
+$(document).ready(function(){
+	
+	var getHeight = $(".get-height").outerHeight();
+	$('.height').height(getHeight);
+	$('.height-padding').height(getHeight-80);
+	$('.heightx2').height(getHeight*2);
+
+	var getHeight2 = $(".get-height2").outerHeight();
+	$('.height2').height(getHeight2);
+	$('.height-padding2').height(getHeight2-80);
+	$('.heightx22').height(getHeight2*2);
+
+});
+
